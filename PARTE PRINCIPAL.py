45a34c558ed738e9e7706437260d88f60a034520
Arcanjo <Arcanjobc@al.insper.edu.br>
2023-11-15 17:47:37 -0300

    oi

--- PARTE PRINCIPAL.py
+++ PARTE PRINCIPAL.py
@@ -1,21 +1,19 @@
 import pygame
 pygame.init()
 
-
 window = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption('NOSSO JOGO')
 
 game = True
 
-
 while game:
     
     for event in pygame.event.get():
-       
+
         if event.type == pygame.KEYDOWN:
             game = False
 
-   
+
     window.fill((0, 71, 171)) 
     pygame.display.update()
 
