2268d62fba7d647fe87400ba75e6e1a9c1ca679f
Bruno N Mota <brunonm@al.insper.edu.br>
2023-11-15 17:37:32 -0300

    estabelecendo parametros

--- PARTE PRINCIPAL.py
+++ PARTE PRINCIPAL.py
@@ -1,21 +1,54 @@
+
 import pygame
+from parametros import *
 pygame.init()
 
+#DEFININDO AS IMAGENS
 
-window = pygame.display.set_mode((1000, 600))
+
+
+#DEFININDO OS TAMANHOS DO PERSONAGEM E DOS ESPINHOS
+
+
+
+# JANELA
+JANELA = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
 pygame.display.set_caption('NOSSO JOGO')
 
+#FUNDO DA IMAGEM
+
+
+#SPRITE DA PLATAFORMA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 game = True
+
+
 
 
 while game:
     
     for event in pygame.event.get():
-       
+    
         if event.type == pygame.KEYDOWN:
             game = False
 
-   
+
     window.fill((0, 71, 171)) 
     pygame.display.update()
 
