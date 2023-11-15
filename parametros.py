29294cebf22593872e234dfec2ca2b4a5c044494
Bruno N Mota <brunonm@al.insper.edu.br>
2023-11-15 17:46:59 -0300

    parametros

---
+++ parametros.py
@@ -0,0 +1,33 @@
+# Estabelecer as figuras
+
+
+#ESTABELECER OS SONS
+
+def parametros()
+# Dados gerais do jogo.
+LARGURA_JANELA = 1000 # Largura da tela - A DEFINIR
+ALTURA_JANELA =  600# Altura da tela - A DEFINIR
+FPS = 60 # Frames por segundo
+
+# Define tamanhos
+LARGURA_ESPINHOS = 30 #A DEFINIR
+ALTURA_ESPINHOS = 20 #A DEFINIR
+ALTURA_JOGADOR = 20 #A DEFINIR
+LARGURA_JOGADOR = 7 #A DEFINIR
+
+
+"""
+# Define algumas variáveis com as cores básicas
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+AMARELO = (255, 255, 0)
+
+"""
+
+# Estados para controle do fluxo da aplicação
+INICIO = 0
+JOGO = 1
+FIM = 2
