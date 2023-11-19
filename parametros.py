edbaf7c40b5d1cb712e0c407a34828a5e9eea888
Arcanjo <Arcanjobc@al.insper.edu.br>
2023-11-19 18:23:45 -0300

    oi

--- parametros.py
+++ parametros.py
@@ -5,7 +5,8 @@ pygame.init()
 
 FPS = 60 # Frames por segundo
 
-
+janela = pygame.display.set_mode((1000, 500))
+pygame.display.set_caption('Hello World!')
 
 ####TAMANHOS
 #JANELA
@@ -35,25 +36,13 @@ LARGURA_MOEDA = 5
 
 # Estabelecer as figuras
 img_personagem = pygame.image.load('imagens_e_sons/imagens/raposa/raposa_andando/Walk (1).png').convert_alpha()
-img_fundo = pygame.image.load('imagens_e_sons/imagens/fundo/Fundo_jogo.png').convert_alpha()
-img_plataformas = pygame.image.load('imagens_e_sons/imagens/plataformas.png').convert_alpha()
-img_moeda = pygame.image.load('imagens_e_sons/imagens/moeda.png').convert_alpha()
+img_fundo = pygame.image.load('imagens_e_sons/fundo/Fundo_jogo.jpg').convert_alpha()
+#img_plataformas = pygame.image.load('imagens_e_sons/imagens/plataformas.png').convert_alpha()
+#img_moeda = pygame.image.load('imagens_e_sons/imagens/moeda.png').convert_alpha()
 
 
 
-#redimensionando as imagens
-#image = pygame.transform.scale(image, (125, 166)) para obter uma nova imagem de 125 X 166 pixels.
-img_fundo = pygame.transform.scale(img_fundo, (LARGURA_FUNDO, ALTURA_FUNDO))
-img_personagem = pygame.transform.scale(img_personagem, (LARGURA_JOGADOR, ALTURA_JOGADOR))
-img_plataformas = pygame.transform.scale(img_plataformas, (LARGURA_PLATAFORMA, ALTURA_PLATAFORMA))
-img_moeda = pygame.transform.scale(img_moeda, (LARGURA_MOEDA, ALTURA_MOEDA))
 
-#ESTABELECER OS SONS
-som_dano =  pygame.mixer.Sound('imagens_e_sons/sons/batida.wav')
-som_caindo = pygame.mixer.Sound('imagens_e_sons/sons/caindo.wav')
-som_fundo = pygame.mixer.music.load('imagens_e_sons/sons/fundo.ogg')
-som_game_over = pygame.mixer.Sound('imagens_e_sons/sons/game_over.wav')
-som_pegando_a_moeda = pygame.mixer.Sound('imagens_e_sons/sons/moeda.wav')
 
 
 #POSIÇÃO INICIAL DO JOGADOR - A DEFINIR
